25a7b2338caf91d2a3d0dd11913f45a4ec31551a
Tuhin-SnapD <tuhincharabarty14@gmail.com>
2025-08-04 21:30:42 +0530

    Add Netlify adapter and configure for server-side rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tuhin-snapd.github.io',
   base: '/',
   output: 'server',
+  adapter: netlify(),
   integrations: [],
   markdown: {
     shikiConfig: {
